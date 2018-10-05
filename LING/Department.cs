f4300961cb63e2f6dc4d5784a7ad375a91755f6a
Ville <vill3ni3mi@gmail.com>
2018-10-05 13:57:54 +0300

    Premade classes

---
+++ LING/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LING
+{
+    public class Department
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        List<Employee> Employees { get; set; }
+        int EmployeeCount => Employees.Count;
+
+        Department()
+        {
+            Employees = new List<Employee>();
+        }
+
+        Department(int id, string name) : this() {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString() => $"{Name} ({EmployeeCount})";
+    }
+}
---
+++ LING/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LING
+{
+    public class Employee
+    {
+        private double _salary;
+        int Id { get; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        Department Department { get; set; }
+        DateTime StartDate { get; set; }
+        DateTime? EndDate { get; set; } 
+        DateTime? DateOfBirth { get; set; }
+        string Name => $"{FirstName} {LastName}";
+        int Age => (DateOfBirth == null) ? 0: (int)((DateTime.Now - DateOfBirth)?.TotalDays / 365.2421875);
+        public Double Salary{
+            set
+            {
+                try
+                {
+                    if (value >= 0)
+                    {
+                        _salary = value;
+                    }
+                }
+                catch
+                {
+                    throw new Exception("Salary can´t be negative.");
+                }
+            }
+            get => Math.Round(_salary, 2);
+        }
+    }
+}
